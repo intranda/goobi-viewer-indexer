1b08a575685080b1ccb362d4fea51bf11d437700
Jan Vonde <jan.vonde@intranda.com>
2017-12-03 19:16:14 +0100

    Revert "evaluate lido:resourceRepresentation first, then lido:resourceID"
    This reverts commit 794a48d8d66e589a7bc55d176c97fd416c9cdc49.

--- goobi-viewer-indexer/src/de/intranda/digiverso/presentation/solr/LidoIndexer.java
+++ goobi-viewer-indexer/src/de/intranda/digiverso/presentation/solr/LidoIndexer.java
@@ -528,12 +528,11 @@ public class LidoIndexer extends AbstractIndexer {
             // TODO parallel processing of pages will required Goobi to put values starting with 1 into the ORDER attribute
         }
 
-        String xpath =
-                "lido:resourceRepresentation[@lido:type='image_master' or lido:resourceRepresentation[@lido:type='http://terminology.lido-schema.org/resourceRepresentation_type/provided_representation'] or @lido:type='http://terminology.lido-schema.org/lido00464' or @lido:type='image_overview']/lido:linkResource";
+        String xpath = "lido:resourceID";
         String filePath = xp.evaluateToString(xpath, eleResourceSet);
         if (filePath == null || filePath.isEmpty()) {
             // TODO configurable
-            xpath = "lido:resourceID";
+            xpath = "lido:resourceRepresentation[@lido:type='image_master' or @lido:type='http://terminology.lido-schema.org/lido00464' or @lido:type='image_overview']/lido:linkResource";
             filePath = xp.evaluateToString(xpath, eleResourceSet);
         }
 
