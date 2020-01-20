a443126d2ed1e6ee3bf66b24ffa7839aee1faf60
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2020-01-20 17:48:59 +0100

    set version to 4.3.3

--- goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/SolrIndexerDaemon.java
+++ goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/SolrIndexerDaemon.java
@@ -36,7 +36,7 @@ public final class SolrIndexerDaemon {
     /** Logger for this class. */
     private static final Logger logger = LoggerFactory.getLogger(SolrIndexerDaemon.class);
 
-    public static final String VERSION = "4.3.0.20200115";
+    public static final String VERSION = "4.3.3.20200120";
     private static final int MIN_SCHEMA_VERSION = 20190924;
     private static final String SCHEMA_VERSION_PREFIX = "goobi_viewer-";
     private static final int DEFAULT_SLEEP_INTERVAL = 1000;
