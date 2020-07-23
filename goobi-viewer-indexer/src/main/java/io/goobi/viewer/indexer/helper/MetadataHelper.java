01f7016b1f11aeee2108c8d56ac5fd337b517f78
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2020-07-23 13:56:30 +0200

    do not log message on INFO; refs #19296

--- goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/helper/MetadataHelper.java
+++ goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/helper/MetadataHelper.java
@@ -1203,7 +1203,7 @@ public class MetadataHelper {
                     if (subfield.getFieldname().startsWith(NormDataImporter.FIELD_URI)) {
                         // Skip values that probably aren't real identifiers or URIs
                         if (fieldValue.length() < 2) {
-                            logger.info("Authority URI too short: {}", fieldValue);
+                            logger.trace("Authority URI too short: {}", fieldValue);
                             continue;
                         }
                         if (NormDataImporter.FIELD_URI.equals(subfield.getFieldname())) {
