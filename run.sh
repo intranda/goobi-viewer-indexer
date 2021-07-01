0c93d5f1855edaade9292b5e2b6c1af4bf2b3676
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2021-07-01 17:55:30 +0200

    add initial Dockerfile and run script

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+set -u
+
+# set solrUrl and viewerUrl from environment variables (defaults are given in Dockerfile) 
+sed -e "s|<solrUrl>.*</solrUrl>|<solrUrl>${SOLR_URL}</solrUrl>|" -e "s|<viewerUrl>.*</viewerUrl>|<viewerUrl>${VIEWER_URL}</viewerUrl>|" -i /opt/digiverso/indexer/solr_indexerconfig.xml
+
+echo "Starting application"
+java -jar /opt/digiverso/indexer/solrIndexer.jar /opt/digiverso/indexer/solr_indexerconfig.xml
