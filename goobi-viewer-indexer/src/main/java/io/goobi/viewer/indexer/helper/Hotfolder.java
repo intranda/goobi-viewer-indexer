6b47534d4944e44cc514b479c54d3affa533db82
Florian Alpers <florian.alpers@intranda.com>
2021-09-17 07:39:51 +0200

    Add "_downloadImages" to supported download folders for mets
    refs #21302

--- goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/helper/Hotfolder.java
+++ goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/helper/Hotfolder.java
@@ -873,6 +873,9 @@ public class Hotfolder {
                     case "_annotations":
                         dataFolders.put(DataRepository.PARAM_ANNOTATIONS, path);
                         break;
+                    case "_downloadimages":
+                        dataFolders.put(DataRepository.PARAM_DOWNLOAD_IMAGES_TRIGGER, path);
+                        break;
                     default:
                         // nothing
                 }
