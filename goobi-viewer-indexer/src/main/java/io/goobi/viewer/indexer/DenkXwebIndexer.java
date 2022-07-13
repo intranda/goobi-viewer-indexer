2b219ff82506854b20deb418fe8a20ca5f87a902
Jan Vonde <jan.vonde@intranda.com>
2022-07-13 09:31:33 +0200

    removes unused imports

--- goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/DenkXwebIndexer.java
+++ goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/DenkXwebIndexer.java
@@ -15,9 +15,7 @@
  */
 package io.goobi.viewer.indexer;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
