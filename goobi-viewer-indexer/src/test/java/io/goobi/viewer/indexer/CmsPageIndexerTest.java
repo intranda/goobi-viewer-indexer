ccc3b9918580026064d9ade8bf7dd49ab8483331
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2023-03-10 17:29:27 +0100

    updated unit test; refs #23746

--- goobi-viewer-indexer/src/test/java/io/goobi/viewer/indexer/CmsPageIndexerTest.java
+++ goobi-viewer-indexer/src/test/java/io/goobi/viewer/indexer/CmsPageIndexerTest.java
@@ -15,20 +15,25 @@
  */
 package io.goobi.viewer.indexer;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import io.goobi.viewer.indexer.helper.Hotfolder;
+import io.goobi.viewer.indexer.helper.SolrSearchIndex;
 import io.goobi.viewer.indexer.model.SolrConstants;
 
 public class CmsPageIndexerTest extends AbstractSolrEnabledTest {
@@ -42,6 +47,17 @@ public class CmsPageIndexerTest extends AbstractSolrEnabledTest {
     }
 
     /**
+     * @see CmsPageIndexer#CmsPageIndexer(Hotfolder)
+     * @verifies set attributes correctly
+     */
+    @Test
+    public void CmsPageIndexer_shouldSetAttributesCorrectly() throws Exception {
+        Assert.assertNotNull(hotfolder);
+        Indexer indexer = new CmsPageIndexer(hotfolder);
+        Assert.assertEquals(hotfolder, indexer.hotfolder);
+    }
+
+    /**
      * @see CmsPageIndexer#addToIndex(Path,boolean,Map)
      * @verifies add record to index correctly
      */
@@ -49,13 +65,29 @@ public class CmsPageIndexerTest extends AbstractSolrEnabledTest {
     public void addToIndex_shouldAddRecordToIndexCorrectly() throws Exception {
         Path cmsFile = Paths.get("src/test/resources/indexed_cms/CMS123.xml");
         assertTrue(Files.isRegularFile(cmsFile));
+        Path indexFile = Paths.get(hotfolder.getHotfolderPath().toString(), "CMS123.xml");
+        Files.copy(cmsFile, indexFile);
+        assertTrue(Files.isRegularFile(indexFile));
 
         Indexer indexer = new CmsPageIndexer(hotfolder);
-        indexer.addToIndex(cmsFile, false, new HashMap<>());
+        indexer.addToIndex(indexFile, false, new HashMap<>());
 
         SolrDocumentList result = hotfolder.getSearchIndex().search(SolrConstants.PI + ":CMS123", null);
         Assert.assertNotNull(result);
         Assert.assertEquals(1, result.size());
-        Assert.assertEquals("CMS123", result.get(0).getFieldValue(SolrConstants.PI));
+        SolrDocument doc = result.get(0);
+
+        assertEquals("CMS123", doc.getFieldValue(SolrConstants.PI));
+        assertEquals("CMS Page Title", doc.getFieldValue(SolrConstants.LABEL));
+        assertEquals("CMS Page Title", SolrSearchIndex.getSingleFieldStringValue(doc, "MD_TITLE"));
+
+        Collection<Object> categories = doc.getFieldValues("MD_CATEGORY");
+        assertNotNull(categories);
+        assertEquals(2, categories.size());
+        assertTrue(categories.contains("cat1"));
+        assertTrue(categories.contains("cat2"));
+
+        assertEquals("one two three", SolrSearchIndex.getSingleFieldStringValue(doc, "MD_TEXT_LANG_EN"));
+        assertEquals("eins zwei drei", SolrSearchIndex.getSingleFieldStringValue(doc, "MD_TEXT_LANG_DE"));
     }
 }
