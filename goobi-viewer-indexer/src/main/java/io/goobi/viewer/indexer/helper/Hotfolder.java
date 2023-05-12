6f7c17ee7a91252b9848a154a2ff23140035de52
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2023-05-12 11:00:20 +0200

    Revert "LogManager.getContext(true); refs #24733"
    This reverts commit 5145c956101d03b31c4790c1e53dde45de8a9e54.

--- goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/helper/Hotfolder.java
+++ goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/helper/Hotfolder.java
@@ -211,7 +211,7 @@ public class Hotfolder {
         }
 
         // Secondary logging appender
-        final LoggerContext context = (LoggerContext) LogManager.getContext(true);
+        final LoggerContext context = (LoggerContext) LogManager.getContext(false);
         secondaryAppender = (SecondaryAppender) context.getConfiguration().getAppenders().get("record");
     }
 
