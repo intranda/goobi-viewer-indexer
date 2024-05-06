e9523bb24d741f02f7738bf8b646fdc7defb8957
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2024-05-06 18:15:33 +0200

    updated unit tests; refs #25946

--- goobi-viewer-indexer/src/test/java/io/goobi/viewer/indexer/helper/ConfigurationTest.java
+++ goobi-viewer-indexer/src/test/java/io/goobi/viewer/indexer/helper/ConfigurationTest.java
@@ -69,7 +69,7 @@ class ConfigurationTest extends AbstractTest {
     void configItemTest() throws Exception {
         List<String> fieldNames =
                 SolrIndexerDaemon.getInstance().getConfiguration().getMetadataConfigurationManager().getListWithAllFieldNames(FileFormat.METS);
-        assertEquals(122, fieldNames.size());
+        assertEquals(100, fieldNames.size());
         List<FieldConfig> fieldConfigList =
                 SolrIndexerDaemon.getInstance().getConfiguration().getMetadataConfigurationManager().getConfigurationListForField("MD_TESTFIELD");
         Assertions.assertNotNull(fieldConfigList);
@@ -135,7 +135,7 @@ class ConfigurationTest extends AbstractTest {
 
     @Test
     void metadataConfigTest() throws Exception {
-        assertEquals(122,
+        assertEquals(100,
                 SolrIndexerDaemon.getInstance()
                         .getConfiguration()
                         .getMetadataConfigurationManager()
