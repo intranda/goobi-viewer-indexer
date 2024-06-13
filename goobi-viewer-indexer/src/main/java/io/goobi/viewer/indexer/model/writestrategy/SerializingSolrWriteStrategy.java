d7047a881ee539d9c3aec537dfddc553fd1d385f
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2024-06-13 16:08:37 +0200

    SerializingSolrWriteStrategy.save(): switched logging output back to DEBUG

--- goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/model/writestrategy/SerializingSolrWriteStrategy.java
+++ goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/model/writestrategy/SerializingSolrWriteStrategy.java
@@ -458,7 +458,7 @@ public class SerializingSolrWriteStrategy extends AbstractWriteStrategy {
      * @return true if save successful; false otherwise
      */
     private boolean save(SolrInputDocument doc, String fileName) {
-        logger.info("Writing '{}'...", fileName);
+        logger.debug("Writing '{}'...", fileName);
         File file = new File(tempFolder.toFile(), fileName);
         try (FileOutputStream fos = new FileOutputStream(file); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(doc);
