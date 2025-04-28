a495edd6e3a353f29eb015e494c45b081d4ed495
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2025-04-28 15:39:40 +0200

    removed unused import; refs #20082

--- goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/WorldViewsIndexer.java
+++ goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/WorldViewsIndexer.java
@@ -26,7 +26,6 @@ import java.nio.file.attribute.FileTime;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
