e1082b52ccb24921772581278aaab83fbdc9d769
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2026-01-13 17:09:48 +0100

    re-enabled adding ACCESSCONDITION values to grouped metadata docs; fixes #27325

--- goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/Indexer.java
+++ goobi-viewer-indexer/src/main/java/io/goobi/viewer/indexer/Indexer.java
@@ -1427,12 +1427,12 @@ public abstract class Indexer {
         }
 
         // Add access conditions
-        // Commented out because inherited access conditions trigger metadata locking where not wanted
-        //        if (!doc.containsKey(SolrConstants.ACCESSCONDITION)) {
-        //            for (String s : indexObj.getAccessConditions()) {
-        //                doc.addField(SolrConstants.ACCESSCONDITION, s);
-        //            }
-        //        }
+        // This should not trigger any false positives with the "locked metadata" functionality. If it does, viewer-core might need an update
+        if (!doc.containsKey(SolrConstants.ACCESSCONDITION)) {
+            for (String s : indexObj.getAccessConditions()) {
+                doc.addField(SolrConstants.ACCESSCONDITION, s);
+            }
+        }
 
         // Add DC values to metadata doc
         if (dcFields != null) {
